b830ca16570a0f244268d54c81dbf77142a0c89c
OscarHerbergBustos <oherbergbustos@gmail.com>
2024-04-23 15:48:52 +0200

    second commit

---
+++ build/precache-manifest.b03e7c37065085eff25336ec8cd1dc50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e59fa065787c9ff7dc683b72d7a6be",
+    "url": "/pokedex-react/index.html"
+  },
+  {
+    "revision": "33c10be0a37eafaba677",
+    "url": "/pokedex-react/static/css/main.8e98e5b0.chunk.css"
+  },
+  {
+    "revision": "1e582701ea90d30a8728",
+    "url": "/pokedex-react/static/js/2.b1cb3a06.chunk.js"
+  },
+  {
+    "revision": "9d0cc3cfc905af4a6940",
+    "url": "/pokedex-react/static/js/3.69ba5426.chunk.js"
+  },
+  {
+    "revision": "33c10be0a37eafaba677",
+    "url": "/pokedex-react/static/js/main.439565cd.chunk.js"
+  },
+  {
+    "revision": "85071708dab739fe8a6d",
+    "url": "/pokedex-react/static/js/runtime~main.e9385625.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Get the root element
+const rootElement = document.getElementById('root');
+
+// Create a root
+const root = ReactDOM.createRoot(rootElement);
+
+// Render the app inside the root
 root.render(
   <React.StrictMode>
     <App />
@@ -16,9 +21,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
