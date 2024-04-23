5558da299ab75ecc08622ef6d0236c09c60765e2
OscarHerbergBustos <oherbergbustos@gmail.com>
2024-04-23 15:32:34 +0200

    Updates

---
+++ precache-manifest.cf4bb5eb5c5c6af040ea3d3e54316525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68658a1e80ee275966febe2e0e916add",
+    "url": "/pokedex-react/index.html"
+  },
+  {
+    "revision": "2953e59085405e55e6e3",
+    "url": "/pokedex-react/static/css/main.8e98e5b0.chunk.css"
+  },
+  {
+    "revision": "d8b1e11d4e078fe54d0b",
+    "url": "/pokedex-react/static/js/2.5d723b98.chunk.js"
+  },
+  {
+    "revision": "9d0cc3cfc905af4a6940",
+    "url": "/pokedex-react/static/js/3.69ba5426.chunk.js"
+  },
+  {
+    "revision": "2953e59085405e55e6e3",
+    "url": "/pokedex-react/static/js/main.a28bf7b3.chunk.js"
+  },
+  {
+    "revision": "85071708dab739fe8a6d",
+    "url": "/pokedex-react/static/js/runtime~main.e9385625.js"
+  }
+]);
